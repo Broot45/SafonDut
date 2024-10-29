6aa55940538eb21134b2f24fc559736d45ee0498
Никита Прищепа <Prisepan45@gmail.com>
2024-10-29 08:56:31 +0500

    Реализован опыт, ожидаются тесты

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -30,38 +30,55 @@ namespace OOP1
         public string Race { get; set; } // Рассовая принадлежность
         public int LVL { get; set; } // Полный уровень существа
         public int Exp {  get; set; } // Опыт существа
+        public int ExpMax { get; set; } // Максимум опыта, при котором происходит повышение уровня
+        public double LVLinc { get; set; } // Коэффициент, на который увеличивается количество опыта, необходимое для повышения опыта
         public int Age { get; set; } // Возраст существа
         public int AgeDays { get; set; } // Число дней со дня рождения
         public bool Corr {  get; set; } // Факт искажённого состояния существа
         public bool Stasys { get; set; } // Факт стазиса (бонусы возраста не начисляются)
-        public int[] LVLmap { get; set; } // 
 
 
 
-        public Create(string Name, string Race) : this(Name, Race, false) { } // Конструктор класса
+        public Create(string Name, string Race) : this (Name, Race, 0, false) { }
+        public Create(string Name, string Race, int LVL) : this(Name, Race, LVL, false) { } // Конструкторы класса
 
 
-        public Create(string Name, string Race, bool Corr) // Конструктор класса второго порядка
+        public Create(string Name, string Race, int LVL, bool Corr) // ===== Конструктор класса второго порядка
         {
             this.Name = Name;
             this.Race = Race;
-            this.LVL = 0;
+            this.LVL = LVL;
             this.Exp = 0;
+            this.ExpMax = 1000;
+            this.LVLinc = 1.2; // Стандартный коэффициент прогрессии опыта
             this.Age = 0;
             this.AgeDays = 0;
             this.Corr = Corr;
             this.Stasys = false;
 
+
             Console.WriteLine($"Было рождено {(Corr ? "искажённое" : "незапятнанное")} существо рассы {this.Race} носящее имя {this.Name}"); // Типа лог
+
+            if (this.LVL > 0) // Расчёт максимального опыта для текущего уровня (выполняется только при спавне)
+            {
+                int templLVL = 0;
+                while (templLVL < this.LVL)
+                {
+                    templLVL++;
+                    this.ExpMax = this.ExpMax * this.LVLinc;
+                }
+
+            }
+
+            Console.WriteLine($"Его уровень: {this.LVL}"); // Типа лог 2
         }
 
 
-
-        public void DayP(int Day) // Для реализащии возможности перемотки времени возраст принимает на вход количество пройденных дней
-            // При реализации високосного года можно просто не прибавлять возраст существам в дополнительные дни
-            // Годы так-же прибавляются по одному, чтобы при резкой перемотке не было кнб пиздеца, превышеный возраст останется в днях, и каждый день будет перетекать в года и рассовые бонусы
+        public void DayP(int Day) // ===== Для реализащии возможности перемотки времени возраст принимает на вход количество пройденных дней
+        // При реализации високосного года можно просто не прибавлять возраст существам в дополнительные дни
+        // Годы так-же прибавляются по одному, чтобы при резкой перемотке не было кнб пиздеца, превышеный возраст останется в днях, и каждый день будет перетекать в года и рассовые бонусы
         {
-            AgeDays += Day;
+            this.AgeDays += Day;
 
             if (AgeDays >= 364) 
             {
@@ -72,6 +89,19 @@ namespace OOP1
                     this.Exp += Day;
                     Console.WriteLine(    
                 } // Получение бонуса за возраст
+            }
+
+        }
+
+        public void giveExp(int Exp) // ====== Получение опыта
+        { 
+            this.Exp += Exp;
+
+            while (this.Exp >= this.ExpMax) 
+            {
+                this.Exp -= this.ExpMax;
+                this.LVL++;
+                this.ExpMax = this.ExpMax * this.LVLinc
             }
         }
 
